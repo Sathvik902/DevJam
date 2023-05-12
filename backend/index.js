3bf1bf33877dd7be876e4d75741823a096259de4
sathvik K <sathvik902@gmail.com>
2023-05-12 22:15:05 +0530

    By sathvik K

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+
+app.get('/',(req,res)=>{
+    res.send("Hello");
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Running on port ${PORT}`);
+})
