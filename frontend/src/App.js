d0498075afd492f80972c7788efa047cbeb1c585
Gurudatta <gurudatta1455@gmail.com>
2023-05-14 10:39:32 +0530

    By Guru

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
-function App() {
-  return (
-    <></>
-  );
-}
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import MainHeader from "./Shared/components/Navigation/Header";
+import ErrorPage from "./Shared/components/Navigation/ErrorPage";
 
+import Home from "./Layout/Home";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <ErrorPage />,
+    children: [{ path: "/login", element: <MainHeader /> }],
+  },
+]);
+function App() {
+  return <RouterProvider router={router} />;
+}
 export default App;
--- frontend/src/Shared/components/Navigation/ErrorPage.js
+++ frontend/src/Shared/components/Navigation/ErrorPage.js
@@ -1,11 +1,17 @@
-import React from 'react'
-
-function ErrorPage() {
+import React from "react";
+import "./Error.css";
+import { FaExclamationTriangle } from "react-icons/fa";
+const ErrorPage = () => {
   return (
-    <div>
-      
+    <div className="page">
+      <FaExclamationTriangle className="icon" />
+      <h1 className="heading">Oops! Something went wrong.</h1>
+      <p className="message">We're sorry, Please try again later.</p>
+      <a href="/" className="button">
+        Back to Homepage
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
